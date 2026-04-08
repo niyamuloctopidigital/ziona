35de46cc714a8561331413c7ff4763e744ebd9b9
Niyamul Islam <niyamul.octopidigital@gmail.com>
2026-04-08 14:34:44 +0600

    Remove old REFeatures section

--- src/pages/RealEstatePage.tsx
+++ src/pages/RealEstatePage.tsx
@@ -1,6 +1,5 @@
 import REHero from '../components/realestate/REHero';
 import REDemos from '../components/realestate/REDemos';
-import REFeatures from '../components/realestate/REFeatures';
 import REWhy from '../components/realestate/REWhy';
 import REPricing from '../components/realestate/REPricing';
 import REContact from '../components/realestate/REContact';
@@ -15,7 +14,6 @@ export default function RealEstatePage() {
     <>
       <REHero onCTA={scrollToContact} />
       <REDemos />
-      <REFeatures onBook={scrollToContact} />
       <REWhy onTry={scrollToContact} />
       <REPricing onGetStarted={scrollToContact} />
       <REContact />
