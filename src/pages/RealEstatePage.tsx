8d98a3d29997da906de976dd0830a19ef585b221
Niyamul Islam <niyamul.octopidigital@gmail.com>
2026-04-08 14:22:25 +0600

    Remove REHowItWorks section

--- src/pages/RealEstatePage.tsx
+++ src/pages/RealEstatePage.tsx
@@ -1,7 +1,6 @@
 import REHero from '../components/realestate/REHero';
 import REDemos from '../components/realestate/REDemos';
 import REFeatures from '../components/realestate/REFeatures';
-import REHowItWorks from '../components/realestate/REHowItWorks';
 import REWhy from '../components/realestate/REWhy';
 import REPricing from '../components/realestate/REPricing';
 import REContact from '../components/realestate/REContact';
@@ -17,7 +16,6 @@ export default function RealEstatePage() {
       <REHero onCTA={scrollToContact} />
       <REDemos />
       <REFeatures onBook={scrollToContact} />
-      <REHowItWorks onBook={scrollToContact} />
       <REWhy onTry={scrollToContact} />
       <REPricing onGetStarted={scrollToContact} />
       <REContact />
